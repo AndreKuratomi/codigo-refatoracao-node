2a868912b5fec3cf7c60ec178e9b03c8539dc056
André Kuratomi <andrekuratomi@gmail.com>
2022-01-20 16:05:42 -0300

    reafactor: server.js file created and port, etc moved to there

--- src/app.js
+++ src/app.js
@@ -1,12 +1,6 @@
 import express from "express";
 
-const port = 3000;
-
-let app = express();
+const app = express();
 app.use(express.json());
-
-app.listen(port, () => {
-  console.log("App running");
-});
 
 export default app;
