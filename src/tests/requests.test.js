fc13e15afa32d132bd4c09e9e8f2672bf632d98a
André Kuratomi <andrekuratomi@gmail.com>
2022-01-24 16:47:48 -0300

    feat: tests done and utils dir created to stock protopypes for tests

--- src/tests/requests.test.js
+++ src/tests/requests.test.js
@@ -2,35 +2,21 @@ import request from "supertest";
 import app from "../app";
 import bcrypt from "bcryptjs";
 
+import {
+  company1,
+  company2,
+  companyResponse1,
+  companyResponse2,
+  dataForLogin,
+  vehicle,
+} from "../utils/prototypes";
+
 jest.mock("uuid", () => ({ v4: () => "uuid" }));
 
-const spyCompare = jest.spyOn(bcrypt, "hashSync");
+const spyCompare = jest.spyOn(bcrypt, "hash");
 spyCompare.mockReturnValue("1234");
 
-const company1 = {
-  name: "André SA",
-  cnpj: "00000000000000",
-  password: "1234",
-  cep: "00000000",
-  address: "Rua X",
-  number: 123,
-  state: "MG",
-  city: "Araxá",
-};
-console.log(company1.password);
-
-const company2 = {
-  name: "André LTDA",
-  cnpj: "11111111111111",
-  password: "1234",
-  cep: "11111111",
-  address: "Rua Y",
-  number: 321,
-  state: "PI",
-  city: "Pedro II",
-};
-
-describe("Request tests", () => {
+describe("Request tests for companies", () => {
   test("Success message", async () => {
     const response = await request(app)
       .post("/companies/register")
@@ -39,27 +25,16 @@ describe("Request tests", () => {
     expect(response.body.message).toBe("Company successfully created");
   });
 
-  test("Create company1", async () => {
-    const response = await request(app)
-      .post("/companies/register")
-      .send(company1)
-      .expect(201);
-    expect(response.body.company.name).toBe("André SA"); //???? por alguma razão não passa da linha 46 por "400 "Bad Request""
-  });
-
   test("Create company2", async () => {
     const response = await request(app)
       .post("/companies/register")
       .send(company2)
       .expect(201);
-    expect(response.body.company.password).toBe("1234"); // não estou entendendo como o spyOn influencia aqui.
-    // expect(spyCompare).toBe(spyCompare.mockReturnValue("1234")); //toStrictEqual é o objeto completo
+    expect(response.body.company.password).toBe("1234");
   });
 
   test("List users", async () => {
     const response = await request(app).get("/companies").expect(200);
-    expect(response.body).toStrictEqual([company1, company2]);
-
-    // ok, como produzir esse response completo ???
+    expect(response.body).toStrictEqual([companyResponse1, companyResponse2]);
   });
 });
